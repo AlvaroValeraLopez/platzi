e94b8903a8690baac1029378b7bffdc8897051c9
avarelop <alvaro.valera@samara.energy>
2023-08-23 07:19:18 +0200

    Se ha añadido información acerca de la creación de funciones con parametros por defecto.

--- curso-ecmascript/01-Functions.js
+++ curso-ecmascript/01-Functions.js
@@ -7,3 +7,28 @@ const square = (num) => {
 }
 
 const square = num => num * num;
+
+
+
+// FUNCIONES CON PARAMETROS POR DEFECTO.
+
+// Antes de ES6.
+function sumar(number1, number2){
+    var _number1 = number1 || 0
+    var _number2 = number2 || 0
+    
+    return _number1 + _number2
+  }
+  
+  sumar(3,4) // 7
+  sumar(3)   // 3
+  sumar()    // 0
+
+  // A partir de ES6.
+  function sumar(number1 = 0, number2 = 0){
+    return number1 + number2
+  }
+  
+  sumar(3,4) // 7
+  sumar(3)   // 3
+  sumar()    // 0
