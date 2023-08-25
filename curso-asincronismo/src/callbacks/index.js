ddc0993a6b4553f4c0879eb3eeb2a62347a7fc65
avarelop <alvaro.valera@samara.energy>
2023-08-25 08:07:22 +0200

    Se ha modificado el código aplicándole buenas prácticas. Se pretende simplificar el código y mejorar su comprensión.

--- curso-asincronismo/src/callbacks/index.js
+++ curso-asincronismo/src/callbacks/index.js
@@ -7,20 +7,18 @@ function fetchData(urlApi, callback){
 
     xhttp.open('GET', urlApi, true); /* Se abre una conexión con nuestra API. */
     xhttp.onreadystatechange = function(event){ /* Permite conocer el estado de la conexión que se ha abierto. */
-        if(xhttp.readyState === 4){
-            /* 
-                0. => No se ha inicializado la conexión.
-                1. => Cargando. Aún no se ha llamado el valor de send.
-                2. => Ya se ejecutó el valor de send.
-                3. => Interactuando con la solicitud.
-                4. => Se ha completado la llamada.
-            */
-            if(XMLHttpRequestUpload.status === 200)
-                callback(null, JSON.parse(xhttp.responseText));
-        }else{
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
-        }
+        /* 
+            0. => No se ha inicializado la conexión.
+            1. => Cargando. Aún no se ha llamado el valor de send.
+            2. => Ya se ejecutó el valor de send.
+            3. => Interactuando con la solicitud.
+            4. => Se ha completado la llamada.
+        */
+        if(xhttp.readyState !== 4)
+            return callback(new Error('Error' + urlApi), null);
+        
+        if(XMLHttpRequestUpload.status === 200)
+            callback(null, JSON.parse(xhttp.responseText));
     }
     xhttp.send();
 }
